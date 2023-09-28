0a5b986a6612691d255622fac68c043d0de68358
daisuke-gk <daidai481.gake@gmail.com>
2023-09-28 23:00:07 +0900

    comment delete

--- main/java/org/example/Main.java
+++ main/java/org/example/Main.java
@@ -2,8 +2,7 @@ package org.example;
 
 import java.util.HashMap;
 import java.util.Map;
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
         Map<Integer,Integer > userMap = new HashMap<>();
@@ -11,7 +10,7 @@ public class Main {
         userMap.put(3, null);
         userMap.put(null, 4);
 
-        for(Map.Entry<Integer, Integer>s : userMap.entrySet()){
+        for(Map.Entry<Integer, Integer> s : userMap.entrySet()){
 
             try{
                 System.out.println(s.getKey() + s.getValue());
